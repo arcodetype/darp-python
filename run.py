bb3fb6ea7ae78d1e10f4d5c4cbc070b3069770a2
Zach <arcodetype@gmail.com>
2025-11-13 08:27:39 -0500

    feat: cdev shell and dictionary user_config

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 import argparse
 import json
 import os
+import subprocess
 import sys
 
-print('Deploying Container Development')
-
+def get_nested(d, keys):
+    for key in keys:
+        d = d.get(key)
+        if d is None:
+            return d
+    return d
 
 def run_deploy(args):
+    print('Deploying Container Development')
     print(args)
     print('Running deploy')
 
@@ -25,20 +31,19 @@ def run_add(args):
             print(f"Not Json {filename}")
             exit
 
-    subdomains = user_config.get('subdomains')
-    if subdomains is None:
-        user_config['subdomains'] = []
-
-    existing_subdomain = next((sd for sd in subdomains if (sd['name'] == args.name or sd['location'] == args.location)), None)
+    existing_subdomain = get_nested(user_config, ['subdomains', args.name])
     if existing_subdomain is not None:
-        print(f"subdomain {existing_subdomain['name']} already exists at {existing_subdomain['location']}")
+        print(f"subdomain {args.name} already exists at {existing_subdomain['location']}")
         sys.exit()
 
+    subdomains = user_config.get('subdomains')
+    if subdomains is None:
+        user_config['subdomains'] = {}
+
     subdomain = {
-        'name': args.name,
         'location': args.location,
     }
-    user_config.get('subdomains').append(subdomain)
+    user_config['subdomains'][args.name] = subdomain
 
     with open(filename, 'w') as f:
         json.dump(user_config, f, indent=4)
@@ -60,23 +65,33 @@ def run_remove(args):
             print(f"Not Json {filename}")
             exit
 
-    subdomains = user_config.get('subdomains')
-    if subdomains is None:
-        user_config['subdomains'] = []
-
-    if len(subdomains) == 0:
+    existing_subdomain = get_nested(user_config, ['subdomains', args.name])
+    if existing_subdomain is None:
         print("No subdomains to remove")
         sys.exit()
 
-    new_subdomains = [sd for sd in subdomains if sd['name'] != args.name]
-    
-    user_config['subdomains'] = new_subdomains
+    del user_config['subdomains'][args.name]
 
     with open(filename, 'w') as f:
         json.dump(user_config, f, indent=4)
 
     print(f"removed '{args.name}'")
         
+
+def run_shell(args):
+    podman_command = [
+        "podman", "run",
+        "--rm",
+        "-it",
+        "--name", "local-go",
+        "-v", f"{os.getcwd()}:/app",
+        args.container_image,
+        "sh"
+    ]
+
+    # Inherits your terminal's stdin/stdout/stderr and TTY.
+    subprocess.run(podman_command, check=True)
+
 
 parser = argparse.ArgumentParser(
     prog='Container Development',
@@ -89,12 +104,16 @@ subparsers = parser.add_subparsers(help='subcommand help')
 parser_deploy = subparsers.add_parser('deploy', help='deploys the environment')
 parser_deploy.set_defaults(func=run_deploy)
 
-parser_add = subparsers.add_parser('add', help='add a subdomain to the container development')
+parser_shell = subparsers.add_parser('shell', help='starts a shell instance')
+parser_shell.add_argument('container_image')
+parser_shell.set_defaults(func=run_shell)
+
+parser_add = subparsers.add_parser('add_subdomain', help='add a subdomain to the container development')
 parser_add.add_argument('-l', '--location', help='the location of the subdomain', required=True)
 parser_add.add_argument('-n', '--name', help='the name of the subdomain', required=True)
 parser_add.set_defaults(func=run_add)
 
-parser_remove = subparsers.add_parser('remove', help='remove a subdomain to the container development')
+parser_remove = subparsers.add_parser('remove_subdomain', help='remove a subdomain to the container development')
 parser_remove.add_argument('-n', '--name', help='the name of the subdomain', required=True)
 parser_remove.add_argument('-l', '--location', help='the location of the subdomain (not used)', required=False)
 parser_remove.set_defaults(func=run_remove)

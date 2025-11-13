7f432f86cba8c33d500462ef4e28364e1dd98f10
Zach <arcodetype@gmail.com>
2025-11-13 16:48:13 -0500

    feat: set domain command

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def get_user_config(filename):
 def run_deploy(args):
     print('Deploying Container Development')
 
-def run_add(args):
+def run_add_subdomain(args):
     filename = '.config.json'
 
     user_config = get_user_config(filename)
@@ -52,7 +52,7 @@ def run_add(args):
 
     print(f"created '{args.name}' at {args.location}")
 
-def run_remove(args):
+def run_remove_subdomain(args):
     filename = '.config.json'
 
     user_config = get_user_config(filename)
@@ -110,6 +110,17 @@ def run_shell(args):
     # Inherits your terminal's stdin/stdout/stderr and TTY.
     subprocess.run(podman_command, check=True)
 
+def run_set_domain(args):
+    filename = '.config.json'
+
+    user_config = get_user_config(filename)
+    user_config['domain'] = args.name
+
+    with open(filename, 'w') as f:
+        json.dump(user_config, f, indent=4)
+
+    print(f"Domain set to  '{args.name}'")
+
 # Command Line Interactions
 
 parser = argparse.ArgumentParser(
@@ -130,25 +141,34 @@ parser_shell.add_argument('environment', help='The name of the environment to st
 parser_shell.add_argument('container_image', help='The container image from which to create the shell instance')
 parser_shell.set_defaults(func=run_shell)
 
+# cdev set
+parser_set = subparsers.add_parser('set', help='set config value')
+subparser_set = parser_set.add_subparsers(dest='set_command', help='set any of the following in the config')
+
+# cdev set domain
+parser_set_domain = subparser_set.add_parser('domain', help='set domain')
+parser_set_domain.add_argument('name', help='the name of the domain')
+parser_set_domain.set_defaults(func=run_set_domain)
+
 # cdev add
-parser_add = subparsers.add_parser('add', help='add to the container development')
+parser_add = subparsers.add_parser('add', help='add to config')
 subparser_add = parser_add.add_subparsers(dest='add_command', help='add any of the following to the config')
 
 # cdev add subdomain
 parser_add_subdomain = subparser_add.add_parser('subdomain', help='add subdomain')
 parser_add_subdomain.add_argument('name', help='the name of the subdomain')
 parser_add_subdomain.add_argument('location', help='the location of the subdomain')
-parser_add_subdomain.set_defaults(func=run_add)
+parser_add_subdomain.set_defaults(func=run_add_subdomain)
 
 # cdev remove
-parser_remove = subparsers.add_parser('rm', help='remove from the container development')
+parser_remove = subparsers.add_parser('rm', help='remove from config')
 subparser_remove = parser_remove.add_subparsers(dest='remove_command', help='remove any of the following from the config')
 
 # cdev remove subdomain
 parser_remove_domain = subparser_remove.add_parser('subdomain', help='remove subdomain')
 parser_remove_domain.add_argument('name', help='the name of the subdomain')
 parser_remove_domain.add_argument('location (optional)', nargs='?', help='the location of the subdomain')
-parser_remove_domain.set_defaults(func=run_remove)
+parser_remove_domain.set_defaults(func=run_remove_subdomain)
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -161,6 +181,9 @@ if args.command == 'add' and args.add_command is None:
     sys.exit()
 elif args.command == 'rm' and args.remove_command is None:
     parser_remove.print_help()
+    sys.exit()
+elif args.command == 'set' and args.set_command is None:
+    parser_set.print_help()
     sys.exit()
 
 if hasattr(args, 'func'):
